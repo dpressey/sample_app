4b07c6c0b836adf651b366e21846b04fe7340faf
Denzel Pressey <pressey90@gmail.com>
2013-11-23 20:09:01 -0500

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,10 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
+    it "should have the title Home" do
+      visit "/static_pages/home"
+      expect(page).to have_title("Ruby on Rails Tutorial | Home")
+    end   
   end
 
   describe "Help page" do
@@ -22,9 +22,9 @@ describe "Static pages" do
       expect(page).to have_content('Help')
     end
 
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    it "should have the title Help" do
+      visit "/static_pages/help"
+      expect(page).to have_title("Ruby on Rails Tutorial | Help")
     end
   end
 
@@ -35,9 +35,9 @@ describe "Static pages" do
       expect(page).to have_content('About Us')
     end
 
-    it "should have the title 'About Us'" do
+    it "should have the title About" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("Ruby on Rails Tutorial | About")
     end
   end
 end
